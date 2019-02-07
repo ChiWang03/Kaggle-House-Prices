da00b7ddb898c9e77a0e7640e9ab09150afbc0d5
chiwang0503 <chiwang1995@gmail.com>
2019-02-06 18:47:59 -0800

    update

--- Principal Component Analysis and Regression/README.md
+++ Principal Component Analysis and Regression/README.md
@@ -1,12 +1,15 @@
 # Kaggle-House-Prices
-House Prices: Advanced Regression Techniques
+## Principal Component Analysis in Python and R
 
-https://www.kaggle.com/c/house-prices-advanced-regression-techniques/data
+Through princpal compoenet analysis we were able to use dimension reduction to see that only 8 principal components show 64% of the variablitiy of the data. 
 
-In this repo I will demonstrate some upsupervised and supervised learning methods to predict house prices 
+However, as you run pca regression with these 8 components or even 15 components the results of the multiple linear regression were aweful. R-sqaured and Adjust R-sqaured were around 0.1.
 
-Principal Component Analysis 
-Principal Component Anlysis Regression
-Partial Least Squares 
+Therefore I took away a component everytime I fitted the linear model and as expected, the first principal component explains most of the variance so it also contributes most to the Adjusted R-squared, which is 0.098 for the 1 model predictor. 
 
-...
+This indicates that the numeric variables in this data set aren't that useful in predicitng the sale prices. To be fair not even PC1 is useful. 
+
+But for statistical inference, we can see that PC1 has large magnitudes in loadings: TotalBsmtSF, X1stFlrSF, GrLivArea,GarageArea, and TotalSF. This explains the size of the house, which indicates that the higher a house scores in these parts the more expensive the house is.
+
+This indeed makes sense.
+
--- Principal Component Analysis and Regression/pcaR.R
+++ Principal Component Analysis and Regression/pcaR.R
@@ -22,6 +22,33 @@ plot(housepca, type="lines")
 # so lets take 8 predicotors explaing 64% of the variability
 
 
+#lets look at some loadings 
+housepca$rotation[,1:4] 
+# By looking at PC1 with a cutoff at 0.3 we can see that 
+# TotalBsmtSF, X1stFlrSF, GrLivArea,GarageArea,TotalSF has a large magnitude
+# PC1 probabily explains the size of the house more or less the higher a house scores in these parts 
+# the more expensive the house it makes a lot of sense as well since the total square feet has the largest magnitude
+
+# PC2 seems LowQualFinSF, EnclosedPorch, Age, RemodAge seems to do with the quality of the house 
+# With the negative loadings meaning if a house scores high in age and low quality finish and even 
+# a large in door porch the price of the price of the house wont be that high 
+
+# PC3 has high magnitude loadings in BsmtFinSF1 and BsmtUnfSF
+# this explains the basement conditions of the house 
+# high negative loadings with BsmtUnfSF means that the more
+# unfinished area of the basement the cheaper the house
+# The higher a house scores on the area of type 1 finishing the higher the price of the house
+# This also makes sense since for a basement to be considered finished it needs to meet most of the conditiosn for a finished house
+# in regards to the lights walls electricty .....
+
+
+# PC4 Has high magnitudes with garage age and LowQualFinSF
+# this seems quite arbbitary so im not sure what we can conclude of PC4 
+# since garage age has postive loadings it may mean the older the garage the higher the price 
+# This doesnt really add up and so is it for lowQualFinSF
+
+# These interpretations would be useful in the latter analysis 
+
 
 #store the scores 
 scr <- housepca$x[,1:8]
@@ -38,6 +65,22 @@ summary(pcregmod)
 # And R squared is extremely low at 0.09818
 # so this kind of concludes that hey not much is very useful and significant for modelling 
 # We may look in to different methods or maybe the numeric variables are the best predictors for this model
+
+# Now maybe we take only what the Kaiser Criterion tells us to 
+scr_Kaiser <- housepca$x[,1:4]
+pcregKaiser <- lm(train$SalePrice ~ scr_Kaiser)
+summary(pcregKaiser)
+
+# And it is not too greate either 
+# lets just take PC1 
+
+scr_PC1<- housepca$x[,1:1]
+pcregPC1 <- lm(train$SalePrice ~ scr_PC1)
+summary(pcregPC1)
+# and we still get a pretty bad result but we do see that PC1 
+# explains most of the variance and it is contriubting to the
+# R squared the most since it didnt change much over subtracting the predictors. 
+
 
 
 
